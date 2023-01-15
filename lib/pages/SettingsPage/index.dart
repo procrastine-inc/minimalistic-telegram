95399ab493a0b1556b38a6a8df2f4d1d7b7d33a3
metamodern <shabanovbunya@gmail.com>
2023-01-15 13:28:19 +0200

    update settings page appbar

--- lib/pages/SettingsPage/index.dart
+++ lib/pages/SettingsPage/index.dart
@@ -5,18 +5,14 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         titleSpacing: 18,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text("Settings"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.qr_code)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+        ],
       ),
     );
   }
