dae3c0aa1cbe2abbdc21f6f7b3d3b66f34894479
metamodern <shabanovbunya@gmail.com>
2023-01-15 15:21:52 +0200

    add menu to settings

--- lib/pages/SettingsPage/index.dart
+++ lib/pages/SettingsPage/index.dart
@@ -11,7 +11,40 @@ class SettingsPage extends StatelessWidget {
         actions: [
           IconButton(onPressed: () {}, icon: const Icon(Icons.qr_code)),
           IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+          PopupMenuButton(
+              itemBuilder: (context) => [
+                    PopupMenuItem(
+                      child: Wrap(
+                        // crossAxisAlignment: CrossAxisAlignment.center,
+                        spacing: 10,
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: const [
+                          Icon(Icons.edit_outlined, color: Colors.black),
+                          Text("Change name"),
+                        ],
+                      ),
+                    ),
+                    PopupMenuItem(
+                      child: Wrap(
+                        spacing: 10,
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: const [
+                          Icon(Icons.add_a_photo_outlined, color: Colors.black),
+                          Text("Select photo or video"),
+                        ],
+                      ),
+                    ),
+                    PopupMenuItem(
+                      child: Wrap(
+                        spacing: 10,
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: const [
+                          Icon(Icons.exit_to_app, color: Colors.black),
+                          Text("Logout"),
+                        ],
+                      ),
+                    ),
+                  ])
         ],
       ),
     );
