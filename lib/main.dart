b86e644f063d290afa2a8eb2d0c25dd10913c310
metamodern <shabanovbunya@gmail.com>
2023-02-24 12:28:42 +0200

    Revert "cleanup"
    This reverts commit 9a8caef6eddaa53c6f23ca64d511942a75ddd32d.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:minimalistic_telegram/pages/SettingsPage/index.dart';
 import 'package:minimalistic_telegram/services/locator.dart';
 import 'package:minimalistic_telegram/services/telegram_service.dart';
 import 'package:minimalistic_telegram/utils/const.dart';
 import 'package:minimalistic_telegram/utils/router.dart' as util_router;
 import 'package:provider/provider.dart';
-
+import 'package:tdlib/td_client.dart';
+import 'components/ChatsList/index.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
   Provider.debugCheckInvalidValueType = null;
+  await TdPlugin.initialize();
   setupLocator();
   runApp(
     MultiProvider(
