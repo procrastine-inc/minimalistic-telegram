149e2f5c947d8fb1cd403fcf312b27a8e8b0efc2
metamodern <shabanovbunya@gmail.com>
2023-02-24 15:04:43 +0200

    hide splash

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart' show GlobalKey, NavigatorState;
 
-const String initRoute = '/splash';
+const String initRoute = '/';
 const String loginRoute = '/login';
 const String otpRoute = '/code_entry';
 const String homeRoute = '/';
