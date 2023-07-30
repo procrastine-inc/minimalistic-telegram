9887de9a6cda5d7a6aa17862efaffbe9a2c98df7
metamodern <shabanovbunya@gmail.com>
2023-07-30 22:35:01 +0300

    wip: update top row, add correct date

--- lib/components/ChatBlock/index.dart
+++ lib/components/ChatBlock/index.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,39 @@ import 'package:minimalistic_telegram/stores/file_store.dart';
 import 'package:palestine_console/palestine_console.dart';
 import 'package:tdlib/td_api.dart' as td_api;
 
+String formatDate(int dateInSeconds, BuildContext context) {
+  final now = DateTime.now();
+  final date = DateTime.fromMillisecondsSinceEpoch(dateInSeconds * 1000);
+  final timeFormat = MediaQuery.of(context).alwaysUse24HourFormat
+      ? DateFormat.Hm() // 24-hour format (e.g., 14:30)
+      : DateFormat.jm(); // 12-hour format (e.g., 2:30 PM)
+
+  final last365Days = now.subtract(const Duration(days: 365));
+
+  if (date.isAfter(last365Days) && date.isBefore(now)) {
+    // Within the last 365 days, show month and day
+    return DateFormat.MMMd()
+        .format(date); // Month and day format (e.g., Jul 15)
+  } else if (date.year == now.year) {
+    if (date.month == now.month && date.day == now.day) {
+      // Today, show time
+      return timeFormat.format(date); // Format based on system settings
+    } else if (date.isAfter(now.subtract(Duration(days: now.weekday - 1))) &&
+        date.isBefore(
+            now.add(Duration(days: DateTime.daysPerWeek - now.weekday)))) {
+      // This week, show the day of the week
+      return DateFormat.E().format(date); // Weekday format (e.g., Mon)
+    } else {
+      // Not within the last 365 days, show day.month.year
+      return DateFormat.yMd()
+          .format(date); // Full date format (e.g., 07/15/2022)
+    }
+  } else {
+    // Not within the last 365 days and not this year, show day.month.year
+    return DateFormat.yMd().format(date); // Full date format (e.g., 07/15/2022)
+  }
+}
+
 class ChatBlock extends StatelessWidget {
   final td_api.Chat chat;
 
@@ -21,14 +55,8 @@ class ChatBlock extends StatelessWidget {
   Widget build(BuildContext context) {
     // debugger();
     return ListTile(
-      leading: ChatAvatar(
-        photo: chat.photo,
-      ),
-      title: ChatTopRow(
-          title: chat.title,
-          date: chat.lastMessage?.date ??
-              chat.draftMessage?.date ??
-              chat.lastReadInboxMessageId),
+      leading: ChatAvatar(photo: chat.photo),
+      title: ChatTopRow(chat: chat),
       subtitle: ChatMessageSubtitle(
           message: chat.lastMessage, draftMessage: chat.draftMessage),
       // tileColor: theme.colorScheme.background,
@@ -118,11 +146,9 @@ class _ChatAvatarState extends State<ChatAvatar> {
 }
 
 class ChatTopRow extends StatelessWidget {
-  final String title;
+  final td_api.Chat chat;
 
-  final int date;
-
-  const ChatTopRow({super.key, required this.title, required this.date});
+  const ChatTopRow({super.key, required this.chat});
 
   @override
   Widget build(BuildContext context) {
@@ -130,8 +156,14 @@ class ChatTopRow extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.baseline,
       textBaseline: TextBaseline.alphabetic,
       children: [
-        Expanded(child: ChatTitle(title: title)),
-        ChatTimeAndStatus(date: date),
+        Expanded(child: ChatTitle(title: chat.title)),
+        const SizedBox(
+          width: 40,
+        ),
+        ChatTimeAndStatus(
+            date: chat.draftMessage?.date ?? chat.lastMessage?.date ?? 0,
+            isOutgoing: chat.lastMessage?.isOutgoing ?? false,
+            sendingState: chat.lastMessage?.sendingState),
       ],
     );
   }
@@ -161,15 +193,15 @@ class ChatTitle extends StatelessWidget {
         Flexible(
             child: Text(
           title,
-          overflow: TextOverflow.ellipsis,
+          overflow: TextOverflow.fade,
           softWrap: false,
         )),
+        const SizedBox(
+          width: 5,
+        ),
         const Icon(
           CupertinoIcons.volume_off,
           size: 14,
-        ),
-        const SizedBox(
-          width: 40,
         ),
       ],
     );
@@ -178,19 +210,27 @@ class ChatTitle extends StatelessWidget {
 
 class ChatTimeAndStatus extends StatelessWidget {
   final int date;
-  const ChatTimeAndStatus({super.key, required this.date});
+
+  final bool isOutgoing;
+
+  final td_api.MessageSendingState? sendingState;
+  const ChatTimeAndStatus(
+      {super.key,
+      required this.date,
+      required this.sendingState,
+      required this.isOutgoing});
 
   @override
   Widget build(BuildContext context) {
+    Print.green(date.toString());
     return Row(
-      children: const [
-        Icon(
+      children: [
+        const Icon(
           Icons.done_all,
           size: 20,
         ),
-        SizedBox(width: 3),
-        // TODO: add INTL
-        Text("14:00"),
+        const SizedBox(width: 3),
+        Text(formatDate(date, context))
       ],
     );
   }
